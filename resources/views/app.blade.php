a231f2277eb863bf1deda992e23758e802cbb067
Dimitar <dvitanov@bitdms.com>
2026-02-01 00:08:34 +0100

    Adding block css/js optimization

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,19 +7,38 @@
         <title inertia>{{ config('app.name', 'Хедерафарм+') }}</title>
         <link rel="icon" href="/assets/img/logo/logo.png">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link rel="stylesheet" href="/assets/css/all-fontawesome.min.css">
-        <link href="https://fonts.bunny.net/css?family=inter:400,500,600,900&display=swap" rel="stylesheet" />
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <!-- Preconnect to external domains -->
+        <link rel="preconnect" href="https://fonts.bunny.net" crossorigin>
+        <link rel="preconnect" href="https://cdn.jsdelivr.net" crossorigin>
+        
+        <!-- Preload critical CSS -->
+        <link rel="preload" href="/assets/css/bootstrap.min.css" as="style">
+        <link rel="preload" href="/assets/css/style.css" as="style">
+        
+        <!-- Critical CSS (Bootstrap + main styles) - loaded synchronously -->
         <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-        <link rel="stylesheet" href="/assets/css/all-fontawesome.min.css">
-        <link rel="stylesheet" href="/assets/css/animate.min.css">
-        <link rel="stylesheet" href="/assets/css/magnific-popup.min.css">
-        <link rel="stylesheet" href="/assets/css/jquery-ui.min.css">
         <link rel="stylesheet" href="/assets/css/style.css">
+        
+        <!-- Non-critical CSS - loaded asynchronously -->
+        <link rel="stylesheet" href="/assets/css/all-fontawesome.min.css" media="print" onload="this.media='all'">
+        <link rel="stylesheet" href="/assets/css/animate.min.css" media="print" onload="this.media='all'">
+        <link rel="stylesheet" href="/assets/css/magnific-popup.min.css" media="print" onload="this.media='all'">
+        <link rel="stylesheet" href="/assets/css/jquery-ui.min.css" media="print" onload="this.media='all'">
+        
+        <!-- Fallback for browsers with JS disabled -->
+        <noscript>
+            <link rel="stylesheet" href="/assets/css/all-fontawesome.min.css">
+            <link rel="stylesheet" href="/assets/css/animate.min.css">
+            <link rel="stylesheet" href="/assets/css/magnific-popup.min.css">
+            <link rel="stylesheet" href="/assets/css/jquery-ui.min.css">
+        </noscript>
+        
+        <!-- Fonts - loaded with display=swap for non-blocking -->
+        <link href="https://fonts.bunny.net/css?family=inter:400,500,600,900&display=swap" rel="stylesheet" media="print" onload="this.media='all'">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" media="print" onload="this.media='all'">
+        
         <!-- Scripts -->
-        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
         @routes
         @vite(['resources/js/app.js'])
         @inertiaHead
@@ -27,16 +46,17 @@
     <body class="font-sans antialiased">
         @inertia
 
+        <!-- JavaScript - jQuery must load first, then plugins -->
         <script src="/assets/js/jquery-3.7.1.min.js"></script>
-        <script src="/assets/js/modernizr.min.js"></script>
-        <script src="/assets/js/bootstrap.bundle.min.js"></script>
-        <script src="/assets/js/imagesloaded.pkgd.min.js"></script>
-        <script src="/assets/js/jquery.magnific-popup.min.js"></script>
-        <script src="/assets/js/isotope.pkgd.min.js"></script>
-        <script src="/assets/js/jquery.appear.min.js"></script>
-        <script src="/assets/js/jquery.easing.min.js"></script>
-        <script src="/assets/js/jquery-ui.min.js"></script>
-        <script src="/assets/js/wow.min.js"></script>
-        <script src="/assets/js/main.js"></script>
+        <script src="/assets/js/modernizr.min.js" defer></script>
+        <script src="/assets/js/bootstrap.bundle.min.js" defer></script>
+        <script src="/assets/js/imagesloaded.pkgd.min.js" defer></script>
+        <script src="/assets/js/jquery.magnific-popup.min.js" defer></script>
+        <script src="/assets/js/isotope.pkgd.min.js" defer></script>
+        <script src="/assets/js/jquery.appear.min.js" defer></script>
+        <script src="/assets/js/jquery.easing.min.js" defer></script>
+        <script src="/assets/js/jquery-ui.min.js" defer></script>
+        <script src="/assets/js/wow.min.js" defer></script>
+        <script src="/assets/js/main.js" defer></script>
     </body>
 </html>
